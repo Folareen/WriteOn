47eb9fc54ca60a39a80747aa29c74a0b3e7ae6ee
Folareen <sakawahab03@gmail.com>
2023-05-27 00:50:22 +0100

    done with server setup

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,21 @@
-console.log('hii')
+const express = require('express');
+const cors = require('cors');
+const { connect } = require('mongoose');
+const connectToDB = require('./src/utils/connectToDB');
+require('dotenv').config();
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+const startServer = async () => {
+    try {
+        await connectToDB(process.env.MONGO_URI)
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        })
+    } catch (error) {
+        console.log(error);
+        console.log('Unable to start server');
+    }
+}
+startServer();
