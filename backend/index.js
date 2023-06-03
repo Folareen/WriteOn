ef9c98567458ac91a2083f9cdd40f0cc435fadfa
Folareen <sakawahab03@gmail.com>
2023-06-03 16:08:53 +0100

    removed verifyToken middleware from user route

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,6 @@ const connectToDB = require('./src/utils/connectToDB');
 require('dotenv').config();
 const authRouter = require('./src/routes/auth');
 const userRouter = require('./src/routes/user')
-const verifyToken = require('./src/middlewares/verifyToken')
 const notFound = require('./src/middlewares/notFound');
 
 
@@ -13,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use('/api/v1', authRouter)
-app.use('/api/v1/user', verifyToken, userRouter)
+app.use('/api/v1/user', userRouter)
 app.use(notFound);
 
 
