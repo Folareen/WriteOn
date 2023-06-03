a52b0b98d1a5978fda5d01ea059ac41e3a6abf5e
Folareen <sakawahab03@gmail.com>
2023-06-03 15:29:35 +0100

    created verifyToken middleware

---
+++ backend/src/middlewares/verifyToken.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken')
+
+const verifyToken =  (req, res, next) => {
+    try{
+        if(!req.headers.authorization){
+            return res.status(401).json({message: 'Unauthorized!'})
+        }
+        
+        const token = req.headers.authorization.split(' ')[1];
+
+        if(!token) {
+            return res.status(401).json({message: 'Unauthorized!'})
+        }
+
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        if(!decodedToken) {
+            return res.status(401).json({message: 'Unathorized!'})
+        }
+
+        next();
+    }catch(err){
+        console.log(err.message)
+        res.status(500).json({message: 'Unable to verify token'})
+    }
+}
+
+module.exports = verifyToken;
