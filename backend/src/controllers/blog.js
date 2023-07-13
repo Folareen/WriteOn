5dcfdf7732e7258b3a4c25aa0f9b9afd93211d3a
Folareen <sakawahab03@gmail.com>
2023-07-13 02:08:02 +0100

    done with edit blog

--- backend/src/controllers/blog.js
+++ backend/src/controllers/blog.js
@@ -1,5 +1,4 @@
 const Blog = require("../models/Blog")
-const getUserFromToken = require("../utils/getUserFromToken")
 
 const getBlogs = async (req, res) => {
     try {
@@ -13,45 +12,89 @@ const getBlogs = async (req, res) => {
 
 const createBlog = async (req, res) => {
     try {
+        const user = req.user
         const { author, title, content, category } = req.body
-        if(!author){
-            return res.status(400).json({message: 'Author is required'})
+        if (!author) {
+            return res.status(400).json({ message: 'Author is required' })
         }
-        if(!title){
-            return res.status(400).json({message: 'Title is required'})
+        if (!title) {
+            return res.status(400).json({ message: 'Title is required' })
         }
-        if(title.length < 3){
-            return res.status(400).json({message: 'Title should be atleast three characters'})
+        if (title.length < 3) {
+            return res.status(400).json({ message: 'Title should be atleast three characters' })
         }
-        if(title.length > 20){
-            return res.status(400).json({message: 'Title should not be more than twenty characters'})
+        if (title.length > 20) {
+            return res.status(400).json({ message: 'Title should not be more than twenty characters' })
         }
-        if(!content){
-            return res.status(400).json({message: 'Content is required'})
+        if (!content) {
+            return res.status(400).json({ message: 'Content is required' })
         }
-        if(!category){
-            return res.status(400).json({message: 'Category is required'})
+        if (!category) {
+            return res.status(400).json({ message: 'Category is required' })
         }
-        
+
         const blogId = title.toLowerCase().split(' ').join('-')
 
-        const user = getUserFromToken(req)
+        const titleExists = await Blog.findOne({
+            id: blogId, author: user._id
+        })
+
+        if(titleExists){
+            return res.status(400).json({message: 'Title in use'})
+        }
 
         const blog = await Blog.create({
             ...req.body,
-            author : user._id, id : blogId
-        }).populate("author")
+            author: user._id, id: blogId
+        })
 
-        res.status(201).json({blog, message: "Blog created successfully"})
+        res.status(201).json({ blog, message: "Blog created successfully" })
     } catch (err) {
         console.log(err.message)
-        res.status(500).json({message: 'Something went wrong'})
+        res.status(500).json({ message: 'Something went wrong' })
     }
 
 }
 
+const editBlog = async (req, res) => {
+    try {
+        const user = req.user
+        const { id } = req.params
+        const blog = await Blog.findOne({id, author: user._id})
+        if(!blog){
+            return res.status(404).json({message: 'Blog not found'})
+        }
+
+        const { title, content, description, category, published } = req.body
+
+        if(title){
+            blog.title = title
+        }
+        if(content){
+            blog.content = content
+        }
+        if(description){
+            blog.description = description
+        }
+        if(category){
+            blog.category = category
+        }
+        if(published != undefined || published != null){
+            blog.published = published
+        }
+
+        await blog.save()
+
+        
+        res.status(200).json({ message: 'Blog updated successfully'})
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).json({ message: 'Something went wrong' })
+    }
+}
 
 module.exports = {
     createBlog,
-    getBlogs
+    getBlogs,
+    editBlog
 }
--- backend/src/routes/blog.js
+++ backend/src/routes/blog.js
@@ -1,10 +1,12 @@
 const {Router} = require("express")
-const {createBlog, getBlogs } = require("../controllers/blog")
+const {createBlog, getBlogs, editBlog } = require("../controllers/blog")
 const verifyToken = require("../middlewares/verifyToken")
 
 const router = Router()
 
 router.post('/', verifyToken, createBlog)
 router.route('/').get(getBlogs)
+router.use(verifyToken)
+router.route('/:id').patch(editBlog)
 
 module.exports = router
