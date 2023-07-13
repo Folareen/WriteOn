56a4f5b8af91c7d5c2216ed61c9ac58dfe7e2bcb
Folareen <sakawahab03@gmail.com>
2023-07-13 01:09:12 +0100

    validated blog object id

--- backend/src/routes/blog.js
+++ backend/src/routes/blog.js
@@ -1,10 +1,12 @@
 const {Router} = require("express")
 const {createBlog, getBlogs } = require("../controllers/blog")
+const validateObjectId = require("../middlewares/validateObjectId")
 const verifyToken = require("../middlewares/verifyToken")
 
 const router = Router()
 
 router.post('/', verifyToken, createBlog)
 router.route('/').get(getBlogs)
+router.param("id", validateObjectId);
 
 module.exports = router
