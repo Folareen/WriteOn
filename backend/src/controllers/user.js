3d5c96c3a5fde99ec57a900e1de29bcb0b5c3bc1
Folareen <sakawahab03@gmail.com>
2023-07-27 18:48:45 +0100

    done with avatar edit

--- backend/src/controllers/user.js
+++ backend/src/controllers/user.js
@@ -1,36 +1,43 @@
 const User = require("../models/User");
 const Blog = require("../models/Blog");
+const cloudinary = require('cloudinary')
 
 const editProfile = async (req, res) => {
-    try{
+    try {
         const { username, firstName, lastName, email } = req.body;
-        if(!username && !firstName && !lastName && !email){
+        if (!username && !firstName && !lastName && !email) {
             return res.sendStatus(400)
         }
         const user = await User.findOne({ _id: req.user._id });
-        if(username){
+        if (username) {
             user.username = username
         }
-        if(firstName){
+        if (firstName) {
             user.firstName = firstName
         }
-        if(lastName){
+        if (lastName) {
             user.lastName = lastName
         }
-        if(email){
+        if (email) {
             user.email = email
         }
+        if (req?.files?.avatar) {
+            const result = await cloudinary.v2.uploader.upload(req.files.avatar.tempFilePath, { folder: 'writeon--user-avatars' })
+            user.avatar = result.secure_url
+        }
         await user.save()
-        
-        res.status(200).json({user : {
-            _id : user._id,
-            username: user.username,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            email: user.email,
-        }, message: 'Profile updated successfully.'})
 
-    } catch(error){
+        res.status(200).json({
+            user: {
+                _id: user._id,
+                username: user.username,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                email: user.email,
+            }, message: 'Profile updated successfully.'
+        })
+
+    } catch (error) {
         console.log(error.message)
         res.status(500).json({ message: 'Something went wrong' })
     }
@@ -45,7 +52,7 @@ const getUser = async (req, res) => {
             return res.status(404).json({ message: 'User not found' })
         }
 
-        const blogs = await Blog.find({author : user._id})
+        const blogs = await Blog.find({ author: user._id })
 
         res.status(200).json({
             user: {
