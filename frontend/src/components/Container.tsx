0b4b03898c9a530fccc82de7eecbd05b09663575
Folareen <sakawahab03@gmail.com>
2023-07-30 18:30:03 +0100

    created normal container

---
+++ frontend/src/components/Container.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type Props = {
+    className: string,
+    children: React.ReactNode
+}
+
+const Container = ({ className, children }: Props) => {
+    return (
+        <div className={`px-6 lg:px-36 ${className}`}>
+            {children}
+        </div>
+    )
+}
+
+export default Container
