d85ee39d5eb39bd4b1825d70517b5ef77537c410
Folareen <sakawahab03@gmail.com>
2023-07-30 18:31:13 +0100

    done with header

---
+++ frontend/src/components/layouts/Header.tsx
@@ -0,0 +1,155 @@
+import React, { useState } from 'react'
+import { MdOutlineMenuOpen, MdOutlineWorkOutline } from 'react-icons/md'
+import Logo from '../Logo'
+import { RiArticleFill, RiCloseCircleLine, RiFileEditFill, RiLogoutCircleLine } from 'react-icons/ri'
+import { Link, useLocation } from 'react-router-dom'
+import { GoHome } from 'react-icons/go'
+import { FcFaq } from 'react-icons/fc'
+import { LuMessagesSquare } from 'react-icons/lu'
+import { AiOutlineLogin } from 'react-icons/ai'
+import { IoMdPersonAdd } from 'react-icons/io'
+import getUrlFromTitle from '../../utils/getUrlFromTitle'
+import Container from '../Container'
+
+const MobileHeader = ({ links, pathname }: { links: string[], pathname: string }) => {
+  const [showNav, setShowNav] = useState(false)
+
+  return <div className='flex lg:hidden p-6 items-center justify-center relative'>
+    <Logo dark={true} />
+    <button onClick={() => {
+      setShowNav(true)
+    }} className='absolute right-6'>
+      <MdOutlineMenuOpen />
+    </button>
+
+    <Container className={`absolute ${showNav ? 'top-0' : 'top-[-100vh]'} left-0 right-0 w-screen bg-[rgba(0,0,0,0.3)] h-screen transition-all duration-500`}>
+      <div className='w-full h-2/5 bg-white rounded-b-3xl'>
+
+        <button onClick={() => {
+          setShowNav(false)
+        }} className='absolute top-10 left-1/2 -translate-x-1/2'>
+          <RiCloseCircleLine />
+        </button>
+
+        <nav className='relative top-16 text-[#1E1E21] font-semibold'>
+          {
+            links.map((item) => {
+
+              if (item == 'Login') {
+                return <Link to={getUrlFromTitle(item)} className={`flex flex-row gap-2 p-2 border-[#1E1E21] border-solid border-0 border-b-[1px] items-center text-[#9333EA] ${pathname == getUrlFromTitle(item) ? 'bg-gray-200' : 'bg-white'}`}>
+                  <AiOutlineLogin />
+                  <p className=' text-base'>
+                    {item}
+                  </p>
+                </Link>
+              }
+
+              if (item == 'Sign up') {
+                return <Link to={getUrlFromTitle(item)} className={`flex flex-row gap-2 p-2 items-center text-[#503668] ${pathname == getUrlFromTitle(item) ? 'bg-gray-200' : 'bg-white'}`}>
+                  <IoMdPersonAdd />
+                  <p className=' text-base'>
+                    {item}
+                  </p>
+                </Link>
+              }
+
+              if (item == 'Logout') {
+                return <button className='flex flex-row items-center gap-2 p-2 text-[#C52727]'>
+                  <RiLogoutCircleLine />
+                  <span>Logout</span>
+                </button>
+              }
+
+              return <Link to={getUrlFromTitle(item)} className={`flex flex-row gap-2 p-2 border-[#1E1E21] border-solid border-0 border-b-[1px] items-center ${pathname == getUrlFromTitle(item) ? 'bg-gray-200' : 'bg-white'}`}>
+                {
+                  item == 'Home' ?
+                    <GoHome />
+                    :
+                    item == 'My Portfolio' ?
+                      <MdOutlineWorkOutline />
+                      :
+                      item == 'Blog' ?
+                        <RiArticleFill />
+                        :
+                        item == 'Create Blog' ?
+                          <RiFileEditFill />
+                          :
+                          item == 'FAQs' ?
+                            <LuMessagesSquare />
+                            :
+                            ''
+
+                }
+                <p className=' text-base'>
+                  {item}
+                </p>
+              </Link>
+            })
+          }
+        </nav>
+      </div>
+    </Container>
+
+
+  </div>
+}
+
+const DesktopHeader = ({ links, pathname }: { links: string[], pathname: string }) => {
+  return <Container className='hidden lg:flex bg-[#26262F] p-[27px] justify-between'>
+    <Logo dark={false} />
+    <div className='flex flex-row gap-5 items-center font-medium'>
+      {
+        links.map((item) => {
+
+          if (item == 'Login') {
+            return <Link to={'/login'} className='px-3 rounded-md bg-[#9333EA] text-white py-1.5'>
+              Login
+            </Link>
+          }
+
+          if (item == 'Sign up') {
+            return <Link to={'/sign-up'} className='px-3 rounded-md bg-[#B09EC0] text-white py-1.5'>
+              Sign up
+            </Link>
+          }
+
+          if (item == 'Create Blog') {
+            return <Link to={'/create-blog'} className='px-3 rounded-md bg-[#9333EA] text-white py-1.5'>
+              Create Blog
+            </Link>
+          }
+
+          if (item == 'Logout') {
+            return <button className='px-3 rounded-md bg-[#C52727] text-white py-1.5'>
+              Logout
+            </button>
+          }
+
+          return <Link to={getUrlFromTitle(item)} className={`text-white ${pathname == getUrlFromTitle(item) || (pathname == '/' && item == 'Home') ? 'font-bold underline underline-offset-1' : ''} hover:underline`}>
+            {item}
+          </Link>
+        })
+      }
+    </div>
+  </Container>
+}
+
+const Header = () => {
+
+  const { pathname } = useLocation()
+
+  const links = false ? [
+    'Home', 'My Portfolio', 'Blog', 'FAQs', 'Create Blog', 'Logout'
+  ] : [
+    'Home', 'Blog', 'FAQs', 'Login', 'Sign up'
+  ]
+
+  return (
+    <header>
+      <MobileHeader links={links} pathname={pathname} />
+      <DesktopHeader links={links} pathname={pathname} />
+    </header>
+  )
+}
+
+export default Header
