986128b2d8b3a774c3fdfdb21ff660ca7a73f45a
Folareen <sakawahab03@gmail.com>
2023-09-12 22:57:43 +0100

    added home link

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div>NotFound <Link to={'/'}>Go home</Link></div>
   )
 }
 
