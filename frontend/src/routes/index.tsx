a243152bccb5266fe7e03309cb1922aa050279e9
Folareen <sakawahab03@gmail.com>
2023-09-13 23:38:01 +0100

    integrated auth state

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,16 +1,32 @@
-import React from "react";
+import { useLayoutEffect } from "react";
 import {
   BrowserRouter as Router,
 } from "react-router-dom";
 import PrivateRoutes from "./PrivateRoutes";
 import PublicRoutes from './PublicRoutes';
+import useAuthStore from "../stores/useAuthStore";
+import jwtDecode from "jwt-decode";
+import { setAxiosToken } from "../api/axios";
 
 const Routes = () => {
-  const isLoggedIn: boolean = false;
+  const { user, authenticate } = useAuthStore()
+
+  useLayoutEffect(() => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      const user = jwtDecode(token)
+      if (user) {
+        setAxiosToken(token)
+        authenticate(user)
+      }
+    } else {
+      authenticate(null)
+    }
+  }, [])
 
   return (
     <Router>
-      {isLoggedIn ? (
+      {user ? (
         <PrivateRoutes />
       ) : (
         <PublicRoutes />
