56f777351a5ef66233c15dad82e8828792407704
Folareen <sakawahab03@gmail.com>
2023-09-13 23:37:03 +0100

    added notfound page to private routes

--- frontend/src/routes/PrivateRoutes.tsx
+++ frontend/src/routes/PrivateRoutes.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Dashboard from '../pages/private/Dashboard'
+import NotFound from '../pages/NotFound'
 
 const PrivateRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   )
 }
