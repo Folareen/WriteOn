1fcaeb973adb5bafe525ee78aabf0ff9124cb96d
Folareen <sakawahab03@gmail.com>
2023-09-15 23:54:49 +0100

    fixed auth persistence issues

--- frontend/src/stores/useAuthStore.ts
+++ frontend/src/stores/useAuthStore.ts
@@ -1,21 +1,27 @@
 import { create } from "zustand";
 import { setAxiosToken } from "../api/axios";
+import jwtDecode from "jwt-decode";
 
 type AuthState = {
     user: any,
-    authenticate: (user: any) => void
+    authenticate: (user: any) => void,
+    logout: () => void
 }
 
 const useAuthStore = create<AuthState>((set) => ({
     user: null,
-    authenticate(user: any){
-        setAxiosToken(user?.accessToken)
+    authenticate(token: string){
+      const user = jwtDecode(token)
+        setAxiosToken(token)
+        localStorage.removeItem('token')
+        localStorage.setItem('token', token)
         set(() => ({
             user
         }))
     },
     logout(){
         setAxiosToken(null)
+        localStorage.removeItem('token')
         set({user: null})
     }
 }))
