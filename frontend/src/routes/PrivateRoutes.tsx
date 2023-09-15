11cdc604b77118ced54880dd619c0ac4e8c60289
Folareen <sakawahab03@gmail.com>
2023-09-15 23:55:43 +0100

    enable redirection from auth page

--- frontend/src/routes/PrivateRoutes.tsx
+++ frontend/src/routes/PrivateRoutes.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import Dashboard from '../pages/private/Dashboard'
 import NotFound from '../pages/NotFound'
+import Landing from '../pages/auth/Landing'
 
 const PrivateRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
+      <Route path="/" element={<Landing />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/login" element={<Navigate to={'/'} />} />
+      <Route path="/signup" element={<Navigate to={'/'} />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   )
