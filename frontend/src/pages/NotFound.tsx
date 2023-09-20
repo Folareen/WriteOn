e1a0f89181dd26321d46c00e016a3a86cc299162
Folareen <sakawahab03@gmail.com>
2023-09-20 11:19:35 +0100

    redesigned 404 page

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -3,7 +3,15 @@ import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
-    <div>NotFound <Link to={'/'}>Go home</Link></div>
+    <div className='flex justify-center items-center w-full h-screen bg-blue-50'>
+      <div>
+        <h2 className='text-[#1E1E21] text-lg md:text-xl text-center mb-4 md:mb-8 font-semibold'>
+          Page not found
+        </h2>
+        <Link to={'/'} className='bg-[#9333EA] text-white text-base md:text-lg font-bold rounded-[5px] md:rounded-[10px] py-[10px] md:py-[15px] px-[60px] md:px-[120px] block mx-auto w-max'>Go home</Link>
+
+      </div>
+    </div>
   )
 }
 
