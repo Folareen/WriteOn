eb1ba6b3d34a228fe338655c359624885e8c3fb4
Folareen <sakawahab03@gmail.com>
2023-09-26 23:56:06 +0100

    test blog card

--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -1,8 +1,26 @@
+import { Link } from "react-router-dom"
 
+type Props = {
+    id: string,
+    coverImage: string,
+    title: string,
+    date: string
+}
 
-const BlogCard = () => {
+const BlogCard = ({ id, coverImage, title, date }: Props) => {
     return <div>
-        BlogCard
+        <img src={coverImage} />
+        <h2>
+            {title}
+        </h2>
+        <div className="flex justify-between items-center">
+            <p className="text-[8px] lg:text-base">
+                {date}
+            </p>
+            <Link to={`/blog/${id}`}>
+                Read More
+            </Link>
+        </div>
     </div>
 }
 
